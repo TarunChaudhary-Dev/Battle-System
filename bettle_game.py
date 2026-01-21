a881be2410b26f7882b279845cf17662e6527db3
Tarun Chaudhary ( The Silent Ghost ) <iamtarunchaudhary.dev@gmail.com>
2026-01-21 08:28:17 +0530

    Add num_of_fighter to track Fighter instances
    Added a class variable to track the number of Fighter instances.

--- bettle_game.py
+++ bettle_game.py
@@ -1,8 +1,11 @@
 class Fighter:
+    num_of_fighter = 0
+    
     def __init__(self,name,health,strength):
         self.name = name
         self.health = health
         self.strength = strength
+        Fighter.num_of_fighter += 1
         
     def attack(self,enemy):      
         enemy.health -= self.strength
@@ -11,6 +14,8 @@ class Fighter:
         
 fighter1 = Fighter("Alpha" , 100 , 20)
 fighter2 = Fighter("Gamma",100 , 10)
+fighter3 = Fighter("Beta", 100 , 30)
 
 fighter1.attack(fighter2)
 fighter2.attack(fighter1)
+print(Fighter.num_of_fighter)
